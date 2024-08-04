15916b0b768852af2f0ab5e19cc11d97968d7ee3
kr-goos <wooseok_3@icloud.com>
2024-08-04 21:40:37 +0900

    golang codingtest Lv1 fruit peddler

--- Lv1/fruit_peddler/solution.go
+++ Lv1/fruit_peddler/solution.go
@@ -1,5 +1,21 @@
 package fruitpeddler
 
+import (
+	"sort"
+)
+
 func solution(k int, m int, score []int) int {
-	return 0
+
+	sort.Slice(score, func(i, j int) bool {
+		return score[i] > score[j]
+	})
+
+	var profit int
+
+	for i := 0; i+m <= len(score); i += m {
+		box := score[i : i+m]
+		profit += box[m-1] * m
+	}
+
+	return profit
 }
--- Lv1/fruit_peddler/solution_test.go
+++ Lv1/fruit_peddler/solution_test.go
@@ -1,1 +1,25 @@
 package fruitpeddler
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	const TESTCOUNT = 2
+	k := [TESTCOUNT]int{3, 4}
+	m := [TESTCOUNT]int{4, 3}
+	score := [TESTCOUNT][]int{
+		{1, 2, 3, 1, 2, 3, 1},
+		{4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2},
+	}
+	success := [TESTCOUNT]int{8, 33}
+
+	for i := 0; i < TESTCOUNT; i++ {
+		fmt.Println("k[i] : ", k[i])
+		fmt.Println("m[i] : ", m[i])
+		fmt.Println("score[i] : ", score[i])
+		fmt.Println("success[i] : ", success[i])
+		fmt.Println("solution result : ", solution(k[i], m[i], score[i]))
+	}
+}
