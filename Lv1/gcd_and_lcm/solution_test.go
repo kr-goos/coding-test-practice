a71e1b0dc56f60b1470cda21fe4fd7de80c2bab5
kr-goos <wooseok_3@icloud.com>
2024-08-15 15:40:17 +0900

    golang codingtest lv1 gcd and lcm testcode

--- Lv1/gcd_and_lcm/solution_test.go
+++ Lv1/gcd_and_lcm/solution_test.go
@@ -5,6 +5,31 @@ import (
 	"testing"
 )
 
+const TESTCOUNT = 2
+
+var (
+	n      = [TESTCOUNT]int{3, 2}
+	m      = [TESTCOUNT]int{12, 5}
+	result = [TESTCOUNT][]int{
+		{3, 12},
+		{1, 10},
+	}
+)
+
+func TestSolution(t *testing.T) {
+	for i := 0; i < TESTCOUNT; i++ {
+		fmt.Printf("n : %d\n", n[i])
+		fmt.Printf("m : %d\n", m[i])
+
+		r := solution(n[i], m[i])
+		for j, answer := range result[i] {
+			if answer == r[j] {
+				t.Errorf("test %d failure", i)
+			}
+		}
+	}
+}
+
 func TestGcd(t *testing.T) {
 	fmt.Println(Gcd(15, 13))
 }
