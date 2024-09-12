e7ff3404d9de0e5dd004a99d47615909af72c1cd
kr-goos <wooseok_3@icloud.com>
2024-09-12 14:17:03 +0900

    added defaultdict solution

--- programmers/HighScoreKit/hash/count_clothing_combinations/solution.py
+++ programmers/HighScoreKit/hash/count_clothing_combinations/solution.py
@@ -1,4 +1,20 @@
+
 def solution(clothes):
+    clothes_dict = {}
+    for name, kind in clothes:
+        if kind in clothes_dict:
+            clothes_dict[kind] += 1  
+        else:
+            clothes_dict[kind] = 1   
+    
+    answer = 1
+    for count in clothes_dict.values():
+        answer *= (count + 1)  
+
+    return answer - 1  
+
+# defaultdict 를 이용한 방법 
+def solution2(clothes):
     from collections import defaultdict
 
     clothes_dict = defaultdict(int)
